c2a9fc54f7b226581d7ab2dff3a852456c57c684
Marklaurence02 <marklaurencecaringal7@gmail.com>
2024-12-01 19:22:51 +0800

    datebase

--- sql/database.sql
+++ sql/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2024 at 05:06 PM
+-- Generation Time: Dec 01, 2024 at 12:22 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
